1925e6b33551e7934fc459edfccae169a79840e8
Moonshroom <51463915+Moonshroom@users.noreply.github.com>
2020-03-15 18:50:30 +0100

    dodanie random_results()

--- podalgorytmy.py
+++ podalgorytmy.py
@@ -1,5 +1,14 @@
 import numpy as np
+import random
 
 def calculate_entropy(data):
     entropy = sum(np.negative(data) * np.log2(data))
     return entropy
+
+def random_results(x, class_, size):
+  results = []
+  for i in range(x):
+    result = [random.randint(1,class_) for j in range(size)]
+    results.append(result)
+    np.array(results)
+  return results

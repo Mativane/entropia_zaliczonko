85c5e80446c156a45d6a5cf7ff3914e5bb52d772
skowronskij <jaksko@st.amu.edu.pl>
2020-03-29 19:33:14 +0200

    Funkcja choose_bests

--- podalgorytmy.py
+++ podalgorytmy.py
@@ -21,8 +21,7 @@ def random_results(x, class_, size):
 def selection(arrays, entropy, k):
     weights = []
     for array in arrays:
-        occurences = [i / len(array) for i in Counter(array).values()]
-        array_entropy = calculate_entropy(occurences)
+        array_entropy = array_to_entropy(array)
         diff = abs(array_entropy - entropy)
         if diff <= 0.03:
             return [array]
@@ -34,13 +33,14 @@ def selection(arrays, entropy, k):
     )
     return result
 
+
 def choose_parents(parents):
     chosen = random.sample(range(len(parents)),2)
     return chosen
 
+
 def create_children(parent_indexes, best_cases):
-    parent1 = best_cases[parent_indexes[0]]
-    parent2 = best_cases[parent_indexes[1]]
+    parent1, parent2 = get_parents(best_cases, parent_indexes)
 
     split_point = int(len(parent1) / 2)
     parent1part1 = parent1[:split_point]
@@ -67,3 +67,38 @@ def mutate(children, classes):
                 new_class = random.choice(classes)
             child[idx] = new_class
     return children
+
+
+def choose_bests(best_cases, parent_idx, children: list, entropy):
+    parents = get_parents(best_cases, parent_idx)
+    current_cases = children.copy()
+    current_cases.extend(parents)
+    cases_entropy = [
+        abs(array_to_entropy(array) - entropy)
+        for array in current_cases
+    ]
+    work_list = cases_entropy.copy()
+    best_arrays = []
+    similar_found = True
+    while len(best_arrays) < 2:
+        min_value = min(work_list)
+        best_array_idx = cases_entropy.index(min_value)
+        best_arrays.append(current_cases[best_array_idx])
+        if min_value <= 0.03:
+            similar_found = True
+            break
+        work_list.remove(min_value)
+    if similar_found:
+        return [best_arrays]
+    return best_arrays
+
+
+def get_parents(cases, indexes):
+    parent1 = cases[indexes[0]]
+    parent2 = cases[indexes[1]]
+    return parent1, parent2
+
+
+def array_to_entropy(array):
+    occurences = [i / len(array) for i in Counter(array).values()]
+    return calculate_entropy(occurences)

14d7e98a547e3440037ca371c0a0184875c5b918
Jakub Skowroński <jakub.skowronski@gis-support.pl>
2020-03-29 15:20:48 +0200

    Selekcja

--- podalgorytmy.py
+++ podalgorytmy.py
@@ -1,5 +1,23 @@
+from collections import Counter
+import random
 import numpy as np
+
 
 def calculate_entropy(data):
     entropy = sum(np.negative(data) * np.log2(data))
     return entropy
+
+
+def selection(arrays, entropy, k):
+    weights = []
+    for array in arrays:
+        occurences = [i / len(array) for i in Counter(array).values()]
+        array_entropy = calculate_entropy(occurences)
+        diff = abs(array_entropy - entropy)
+        weights.append(1 - diff / entropy)
+    result = random.choices(
+        arrays,
+        weights,
+        k=k
+    )
+    return result
